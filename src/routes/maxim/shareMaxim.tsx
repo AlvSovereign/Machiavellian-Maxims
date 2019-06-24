50f430d82067fcd62677db9867567d73c7fb233c
Alvin Sovereign <alvin@homelyfe.com>
2019-06-24 17:39:28 +0100

    Canvas looking reasonable

--- src/routes/maxim/shareMaxim.tsx
+++ src/routes/maxim/shareMaxim.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import Konva from 'konva';
-import { Stage, Layer, Text } from 'react-konva';
+import { Stage, Layer, Text, Group, Rect } from 'react-konva';
 import { Modal, Paper, makeStyles } from '@material-ui/core';
+import { useTheme } from '@material-ui/styles';
+
+function useClientRect() {
+	const [rect, setRect] = React.useState<any>(null);
+	const [node, setNode] = React.useState<any>(null);
+	const ref: any = React.useCallback((node: any) => {
+		console.log('node: ', node);
+		if (node !== null) {
+			setNode(node);
+			setRect(node.getClientRect());
+		}
+	}, []);
+	return [{ rect, node }, ref];
+}
 
 const _ShareMaxim: React.FC<IProps> = ({
 	media = 'default',
@@ -12,6 +26,17 @@ const _ShareMaxim: React.FC<IProps> = ({
 		height: 1080,
 		width: 1080
 	});
+	const theme: any = useTheme();
+	const [attributionNode, attributionRef] = useClientRect();
+	const [dividerNode, dividerRef] = useClientRect();
+	const [maximNode, maximRef] = useClientRect();
+	const [titleNode, titleRef] = useClientRect();
+
+	React.useEffect(() => {
+		console.log('dividerNode: ', dividerNode);
+		console.log('maximNode: ', maximNode);
+		console.log('attributionNode: ', attributionNode);
+	}, [maximNode]);
 
 	const useStyles = makeStyles({
 		root: {
@@ -41,21 +66,92 @@ const _ShareMaxim: React.FC<IProps> = ({
 			<Paper className={classes.root}>
 				<Stage width={dimensions.width} height={dimensions.height}>
 					<Layer>
-						<Text
-							align={'center'}
-							fontSize={24}
-							fontFamily={'Times'}
-							height={dimensions.height / 2}
-							lineHeight={2}
-							text={`Undermining is personal, refutation isn’t. Refutation communicates “I believe you’re wrong due to the findings of the available evidence”. Undermining communicates “I’m going to humiliate you because your opinions invoke my disdain.” Refutations are logical retorts, undermining is interrelational violence. Learn to distinguish between the two, for they oft appear similar.
-							ILLIMUTABLEMEN.COM
-								`}
-							verticalAlign={'middle'}
-							width={dimensions.width / 2}
-							wrap={'word'}
-							x={dimensions.width / 4}
-							y={dimensions.height / 4}
-						/>
+						<Group>
+							<Text
+								ref={titleRef}
+								align={'right'}
+								fill={theme.palette.primary.main}
+								fontFamily={'Times'}
+								fontSize={34}
+								lineHeight={2}
+								text={'Maxim #122'}
+								verticalAlign={'top'}
+								width={dimensions.width / 2}
+								X={
+									maximNode &&
+									maximNode.rect &&
+									dimensions.width / 2 - maximNode.rect.width / 2
+								}
+								y={
+									maximNode &&
+									maximNode.rect &&
+									dimensions.height / 2 - maximNode.rect.height / 2 - 66
+								}
+							/>
+							<Text
+								ref={maximRef}
+								align={'center'}
+								fontSize={24}
+								fontFamily={'Times'}
+								// fontFamily={'CalendasPlus'}
+								lineHeight={2}
+								text={`We’re all players in a game. You’re a player or a piece on the board, you move or you’re moved. You play the game, or the game plays you. We’re all players in a game. You’re a player or a piece on the board, you move or you’re moved. You play the game, or the game plays you.`}
+								verticalAlign={'middle'}
+								width={dimensions.width / 2}
+								wrap={'word'}
+								x={
+									maximNode &&
+									maximNode.rect &&
+									dimensions.width / 2 - maximNode.rect.width / 2
+								}
+								y={
+									maximNode &&
+									maximNode.rect &&
+									dimensions.height / 2 - maximNode.rect.height / 2
+								}
+							/>
+
+							<Rect
+								ref={dividerRef}
+								align={'center'}
+								fill={theme.palette.primary.main}
+								height={1}
+								width={dimensions.width / 4}
+								x={
+									dividerNode &&
+									dividerNode.rect &&
+									dimensions.width / 2 - dividerNode.rect.width / 2
+								}
+								y={
+									maximNode &&
+									maximNode.rect &&
+									maximNode.node &&
+									dimensions.height / 2 + maximNode.rect.height / 2 + 28
+								}
+							/>
+							<Text
+								ref={attributionRef}
+								align={'center'}
+								fill={'rgba(0, 0, 0, 0.87)'}
+								fontFamily={'Times'}
+								fontSize={34}
+								lineHeight={2}
+								text={'ILLIMUTABLEMEN.COM'}
+								verticalAlign={'middle'}
+								width={dimensions.width / 2}
+								x={
+									attributionNode &&
+									attributionNode.rect &&
+									dimensions.width / 2 - attributionNode.rect.width / 2
+								}
+								y={
+									maximNode &&
+									maximNode.rect &&
+									maximNode.node &&
+									dimensions.height / 2 + maximNode.rect.height / 2 + 28 + 28
+								}
+							/>
+						</Group>
 					</Layer>
 				</Stage>
 			</Paper>
