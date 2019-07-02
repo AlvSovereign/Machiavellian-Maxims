2129d56e592c1f0e6212b58ce7e4f273fd62afee
Alvin Sovereign <alvin@homelyfe.com>
2019-07-02 23:55:33 +0100

    finally sorted out font loading for canvas

--- src/components/ShareMaxim.tsx
+++ src/components/ShareMaxim.tsx
@@ -4,6 +4,7 @@ import { Stage, Layer, Text, Group, Rect } from 'react-konva';
 import { Modal, Paper, makeStyles } from '@material-ui/core';
 import { useTheme } from '@material-ui/styles';
 import imageSizes from '../utils/image-sizes';
+import FontFaceObserver from 'fontfaceobserver';
 
 function useClientRect() {
 	const [rect, setRect] = React.useState<any>(null);
@@ -33,7 +34,21 @@ const _ShareMaxim: React.FC<IProps> = ({
 	const [titleNode, titleRef] = useClientRect();
 	const [groupNode, groupRef] = useClientRect();
 
-	React.useEffect(() => {}, [groupNode]);
+	// Load our custom font first and control rendering of the canvas.
+	// This solves the issue of the canvas rendering before the custom
+	// font has been loaded, thus showing the back up 'Times' font instead.
+	const [fontReady, setFontReady] = React.useState<boolean>(false);
+	function loadFont() {
+		const font = new FontFaceObserver('CalendasPlus');
+
+		font.load().then(() => {
+			setFontReady(true);
+		});
+	}
+
+	React.useEffect(() => {
+		loadFont();
+	}, [fontReady]);
 
 	const useStyles = makeStyles({
 		root: {
@@ -54,110 +69,108 @@ const _ShareMaxim: React.FC<IProps> = ({
 		}
 	}
 
-	React.useLayoutEffect(() => {
-		getDimensions(media);
-	}, [media]);
-
 	return (
 		<Modal open={openModal} onClose={() => closeModal()}>
 			<Paper className={classes.root}>
-				<Stage height={dimensions.height} width={dimensions.width}>
-					<Layer>
-						<Rect
-							fill={'white'}
-							height={dimensions.height}
-							width={dimensions.width}
-							x={0}
-							y={0}
-						/>
-						<Group ref={groupRef} absolutePosition={{ x: 0, y: -28 }}>
-							<Text
-								ref={titleRef}
-								align={'right'}
-								fill={theme.palette.primary.main}
-								fontFamily={'Times'}
-								fontSize={34}
-								lineHeight={2}
-								text={'Maxim #122'}
-								verticalAlign={'top'}
-								width={dimensions.width / 2}
-								X={
-									maximNode &&
-									maximNode.rect &&
-									dimensions.width / 2 - maximNode.rect.width / 2
-								}
-								y={
-									maximNode &&
-									maximNode.rect &&
-									dimensions.height / 2 - maximNode.rect.height / 2 - 66
-								}
-							/>
-							<Text
-								ref={maximRef}
-								align={'center'}
-								fontSize={24}
-								fontFamily={'Times'}
-								// fontFamily={'CalendasPlus'}
-								lineHeight={2}
-								text={`We’re all players in a game. You’re a player or a piece on the board, you move or you’re moved. You play the game, or the game plays you. We’re all players in a game. You’re a player or a piece on the board, you move or you’re moved. You play the game, or the game plays you. `}
-								verticalAlign={'middle'}
-								width={dimensions.width / 2}
-								wrap={'word'}
-								x={
-									maximNode &&
-									maximNode.rect &&
-									dimensions.width / 2 - maximNode.rect.width / 2
-								}
-								y={
-									maximNode &&
-									maximNode.rect &&
-									dimensions.height / 2 - maximNode.rect.height / 2
-								}
-							/>
-
+				{fontReady && (
+					<Stage height={dimensions.height} width={dimensions.width}>
+						<Layer>
 							<Rect
-								ref={dividerRef}
-								align={'center'}
-								fill={theme.palette.primary.main}
-								height={1}
-								width={dimensions.width / 4}
-								x={
-									dividerNode &&
-									dividerNode.rect &&
-									dimensions.width / 2 - dividerNode.rect.width / 2
-								}
-								y={
-									maximNode &&
-									maximNode.rect &&
-									maximNode.node &&
-									dimensions.height / 2 + maximNode.rect.height / 2 + 28
-								}
+								fill={'white'}
+								height={dimensions.height}
+								width={dimensions.width}
+								x={0}
+								y={0}
 							/>
-							<Text
-								ref={attributionRef}
-								align={'center'}
-								fill={'rgba(0, 0, 0, 0.87)'}
-								fontFamily={'Times'}
-								fontSize={34}
-								lineHeight={2}
-								text={'ILLIMUTABLEMEN.COM'}
-								verticalAlign={'middle'}
-								width={dimensions.width / 2}
-								x={
-									attributionNode &&
-									attributionNode.rect &&
-									dimensions.width / 2 - attributionNode.rect.width / 2
-								}
-								y={
-									maximNode &&
-									maximNode.rect &&
-									maximNode.node &&
-									dimensions.height / 2 + maximNode.rect.height / 2 + 28 + 28
-								}
-							/>
-						</Group>
-					</Layer>
-				</Stage>
+							<Group ref={groupRef} absolutePosition={{ x: 0, y: -28 }}>
+								<Text
+									ref={titleRef}
+									align={'right'}
+									fill={theme.palette.primary.main}
+									fontFamily={'CalendasPlus'}
+									fontSize={34}
+									lineHeight={2}
+									text={'Maxim #122'}
+									verticalAlign={'top'}
+									width={dimensions.width / 2}
+									X={
+										maximNode &&
+										maximNode.rect &&
+										dimensions.width / 2 - maximNode.rect.width / 2
+									}
+									y={
+										maximNode &&
+										maximNode.rect &&
+										dimensions.height / 2 - maximNode.rect.height / 2 - 66
+									}
+								/>
+								<Text
+									ref={maximRef}
+									align={'center'}
+									fontSize={24}
+									// fontFamily={'Times'}
+									fontFamily={'CalendasPlus'}
+									lineHeight={2}
+									text={`We’re all players in a game. You’re a player or a piece on the board, you move or you’re moved. You play the game, or the game plays you. We’re all players in a game. You’re a player or a piece on the board, you move or you’re moved. You play the game, or the game plays you.`}
+									verticalAlign={'middle'}
+									width={dimensions.width / 2}
+									wrap={'word'}
+									x={
+										maximNode &&
+										maximNode.rect &&
+										dimensions.width / 2 - maximNode.rect.width / 2
+									}
+									y={
+										maximNode &&
+										maximNode.rect &&
+										dimensions.height / 2 - maximNode.rect.height / 2
+									}
+								/>
+
+								<Rect
+									ref={dividerRef}
+									align={'center'}
+									fill={theme.palette.primary.main}
+									height={1}
+									width={dimensions.width / 4}
+									x={
+										dividerNode &&
+										dividerNode.rect &&
+										dimensions.width / 2 - dividerNode.rect.width / 2
+									}
+									y={
+										maximNode &&
+										maximNode.rect &&
+										maximNode.node &&
+										dimensions.height / 2 + maximNode.rect.height / 2 + 28
+									}
+								/>
+								<Text
+									ref={attributionRef}
+									align={'center'}
+									fill={'rgba(0, 0, 0, 0.87)'}
+									fontFamily={'Times'}
+									fontSize={34}
+									lineHeight={2}
+									text={'ILLIMUTABLEMEN.COM'}
+									verticalAlign={'middle'}
+									width={dimensions.width / 2}
+									x={
+										attributionNode &&
+										attributionNode.rect &&
+										dimensions.width / 2 - attributionNode.rect.width / 2
+									}
+									y={
+										maximNode &&
+										maximNode.rect &&
+										maximNode.node &&
+										dimensions.height / 2 + maximNode.rect.height / 2 + 28 + 28
+									}
+								/>
+							</Group>
+						</Layer>
+					</Stage>
+				)}
 			</Paper>
 		</Modal>
 	);
